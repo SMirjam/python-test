015e3fa539deb0ccbcceaef5dcedaa3e189f1a85
Mirjam Schocher <mail@mirjamschocher.ch>
2019-03-17 15:18:31 +0100

    added responses

--- mi6.py
+++ mi6.py
@@ -1,5 +1,13 @@
+# Begrüssung durch mi6
 for i in range(1,10):
     if i == 7:
         print("Good morning, James Bond")
     else:
         print("Good morning, %03d" % i)
+
+# Antworten der Agenten
+for i in range(1,10):
+    if i == 7:
+        print("James Bond: Good morning, M")
+    else:
+        print("%03d: Good morning, M." % i)
